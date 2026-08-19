22a219cdb02735f7270405ad93440b65b5fad860
DoHoonKim <dohoon1097819@gmail.com>
2026-08-19 11:39:34 +0900

    2026/08/19

---
+++ Leetcode/525.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        balance = {0: 0}
+        num_zeros = 0
+        num_ones = 0
+        longest_subarr_lens = []
+        for end in range(1, len(nums) + 1):
+            if nums[end - 1] == 0:
+                num_zeros += 1
+            else:
+                num_ones += 1
+            if (index := balance.get(num_zeros - num_ones)) is not None:
+                longest_subarr_lens.append(end - index)
+            else:
+                balance[num_zeros - num_ones] = end
+                longest_subarr_lens.append(0)
+        return max(longest_subarr_lens)
+        
